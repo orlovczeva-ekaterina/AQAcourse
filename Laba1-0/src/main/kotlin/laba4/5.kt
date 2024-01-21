b6b65f5ebab9e67fd31054ed5d4d73e7f5547dd5
orlovczeva <orlovczeva@surf.dev>
2024-01-21 19:39:33 +0300

    Пятое задание четвертой темы ООП

---
+++ Laba1-0/src/main/kotlin/laba4/5.kt
@@ -0,0 +1,42 @@
+package laba4
+
+/* *   Пятая лаба.
+ * *   Задание 5: В классе Person добавьте custom геттеры и сеттеры для свойств name и age.
+ * *   Убедитесь, что вы можете управлять доступом к этим свойствам извне класса.
+ * *   Например, вы можете реализовать проверку возраста в сеттере,
+ * *   чтобы убедиться, что возраст неотрицательный.
+ * */
+
+class Person {
+    var name: String = ""
+        set(value) {
+            println("Setting name to $value")
+            field = value
+        }
+        get() {
+            println("Getting name")
+            return field
+        }
+    var age: Int = 0
+        set(value) {
+            if ((value > 0) and (value < 110)) {
+                println("Setting age to $value")
+                field = value
+            } else {
+                println("Возраст не может быть отрицательным либо нереалистично большим")
+            }
+        }
+        get() {
+            println("Getting age")
+            return field
+        }
+}
+
+fun main() {
+    val kate = Person()
+    kate.name = "Kate"
+    kate.age = 24
+    kate.age = 583
+    println("Имя: ${kate.name}")
+    println("Возраст: ${kate.age}")
+}
