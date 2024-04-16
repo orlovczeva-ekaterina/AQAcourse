07221d78a03f30e815901baf0e1f209ce70089a0
orlovczeva-ekaterina <89384801+orlovczeva-ekaterina@users.noreply.github.com>
2024-04-16 20:23:50 +0300

    Update Laba1-0/src/main/kotlin/laba5/1.kt
    Co-authored-by: Vladislav Voronin <voronin@surf.dev>

--- Laba1-0/src/main/kotlin/laba5/1.kt
+++ Laba1-0/src/main/kotlin/laba5/1.kt
@@ -8,7 +8,8 @@ package laba5
 
 val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 fun main() {
-    val n = numbers.filter { it % 2 == 0 }
-    val m = n.map { it * 2 }
+    val m = numbers
+    	.filter { it % 2 == 0 }
+	.map { it * 2 }
     println(m)
 }
